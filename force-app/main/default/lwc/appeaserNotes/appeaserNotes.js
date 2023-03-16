5267d8b210dfb87510c5b7b2791a40a32f8d431e
MuffinGlaze <127454511+MuffinGlaze@users.noreply.github.com>
2023-03-16 19:37:23 -0400

    Initialcommit

---
+++ force-app/main/default/lwc/appeaserNotes/appeaserNotes.js
@@ -0,0 +1,7 @@
+import { LightningElement, api } from 'lwc';
+
+export default class AppeaserNotes extends LightningElement {
+    @api valueFromParentComponent;
+//https://salesforce.stackexchange.com/questions/328309/save-save-new-button-in-lwc
+//https://techdicer.com/how-to-add-remove-rows-in-data-table-in-lightning-web-componentlwc-salesforce/ for reminder maybe
+}
