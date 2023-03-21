bd6b8b1baed836a57a12dae7c6d2b70438ed85b5
yangpatrick97 <127454482+yangpatrick97@users.noreply.github.com>
2023-03-21 19:30:31 -0400

    More syntax

--- force-app/main/default/lwc/appeaserReminder/appeaserReminder.js
+++ force-app/main/default/lwc/appeaserReminder/appeaserReminder.js
@@ -107,35 +107,3 @@ export default class AppeaserReminder extends LightningElement {
         this.dispatchEvent(toastEvent);
     }
 }
-/**
-       * Case: The datatable should also retrieve tasks when it renders.
-       * 
-       * Solution: 
-       * 1. Set up Columns to mirror task field api names
-       * 2. Create an apex class with a method that returns a List of Tasks
-       * 3. import that method into lwc and call it from the connectedCallback()
-       * 4. set this.data = the response of the apex method in the JS
-       */
-
-
-      /**
-       * Case: I want to push a button and create a task in Salesforce and push it to the datatable. 
-       * 
-       * Solution: 
-       * 1. It should call a function that creates a task and push it to this.data
-       *    var task = {
-          name: `Name (${index})`,
-          website: 'www.salesforce.com',
-          amount: Math.floor(Math.random() * 100),
-          phone: `${Math.floor(Math.random() * 9000000000) + 1000000000}`,
-          closeAt: new Date(
-              Date.now() + 86400000 * Math.ceil(Math.random() * 20)
-          ),
-        };
-        this.data.push(task);
-      
-       * 0.8. Create another method in apex class that takes values and creates a task and inserts into database
-       * 0.9. Import method into JS
-       * 2. Call method from Javascript and send task field values as parameters
-       * 3. Display a toast message 
-       */
