6b47f778ad71a1213217ca3519ef33160d3a7d6d
yangpatrick97 <127454482+yangpatrick97@users.noreply.github.com>
2023-03-23 16:02:20 -0400

    Improved readability. Reminder delete is WIP

--- force-app/main/default/lwc/appeaserReminder/appeaserReminder.js
+++ force-app/main/default/lwc/appeaserReminder/appeaserReminder.js
@@ -2,17 +2,16 @@ import { LightningElement, api, wire, track } from 'lwc';
 import getTaskData from '@salesforce/apex/reminderController.getTaskData';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-
 const columns = [
     { label: 'Subject', fieldName: 'Name' },
     { label: 'Due Date', fieldName: 'EndDate', type: 'date' },
 ];
+
 export default class AppeaserReminder extends LightningElement {
     @api valueFromParentComponent;
 
-    @track data = [];
-    @track columns = columns;
-    //https://salesforce.stackexchange.com/questions/355193/how-do-i-display-field-values-from-a-related-objects-in-my-lightning-data-table
+    data = [];
+    columns = columns;
     //Wire Apex class that retrieves Campaign data, and updates data table
     @wire(getTaskData)
     wiredTasks({ error, data }) {
@@ -35,75 +34,43 @@ export default class AppeaserReminder extends LightningElement {
         }
     }
     //Opening and Closing Modal
-    @track customFormModal = false;
+    customFormModal = false;
+
     customShowModalPopup() {            
         this.customFormModal = true;
     }
-  
+
     customHideModalPopup() {    
-        
         this.customFormModal = false;
     }
   
     //pushing tasks/campaigns to salesforce 
-    saveAndNew = false;
-
     handleSave() {
-        this.saveAndNew = false;
         this.handleRecordSave();
-    }
-
-    handleSaveAndNew() {
-        this.saveAndNew = true;
-        this.handleRecordSave();
-    }
-
-    handleReset(event) {
-        const inputFields = this.template.querySelectorAll(
-            'lightning-input-field'
-        );
-        if (inputFields) {
-            inputFields.forEach(field => {
-            field.reset();
-            });
-        }
-    }
-
-    handleSuccess() {
-        if(this.saveAndNew) {
-            this.handleReset();
-        } else {
-            this[NavigationMixin.Navigate]({
-            type: 'standard__recordPage',
-            attributes: {
-                recordId: this.activityId,
-                objectApiName: 'GRA_RV_Rig_Verification__c',
-                actionName: 'view'
-            },
-            });
-        }
     }
 
     handleRecordSave() {
         this.template.querySelector('lightning-record-edit-form').submit(this.fields);
-        this.customHideModalPopup()
-        //refreshApex(this.getTaskData); *NOT WORKING FIGURE OUT REFRESH UPON SAVE
-        .then(result => {
-            if(result){
-                this.showNotification();
-            }
-        })
-        .catch(error => {
-            console.log(error);
-        });
+        this.customHideModalPopup();
+        this.dispatchEvent(
+            new ShowToastEvent({
+            title: 'Success',
+            message: 'Reminder is Saved!',
+            variant: 'success'
+            }))
+            //NEED TO FIGURE OUT REFRESHAPEX
     }
 
-    showNotification() {
-        const toastEvent = new ShowToastEvent({
-            title: 'Success!',
-            message: 'Reminder is Saved',
-            variant: 'success',
+    //Deleting selected reminders
+    reminderSelectedRows = [];
+
+    handleRowSelection(event) {
+        this.reminderSelectedRows = event.detail.selectedRows;
+    }
+ 
+    handleDelete() {
+        this.reminderSelectedRows.map(reminder => {
+            deleteRecord(reminder.Id);
         });
-        this.dispatchEvent(toastEvent);
     }
 }
--- force-app/main/default/lwc/appeaserRestaurantData/appeaserRestaurantData.js
+++ force-app/main/default/lwc/appeaserRestaurantData/appeaserRestaurantData.js
@@ -1,6 +1,11 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import findRestaurants from '@salesforce/apex/RestaurantRecs.findRestaurants';
+import RESTAURANT_OBJECT from '@salesforce/schema/Restaurant__c';
+import { refreshApex } from '@salesforce/apex';
+import LightningModal from 'lightning/modal';
+
+
 /*import NAME_FIELD from '@salesforce/schema/restaurant.name';
 import CUISINE_FIELD from '@salesforce/schema/restaurant.cuisine';
 import PRICE_FIELD from '@salesforce/schema/restaurant.price';
@@ -9,34 +14,36 @@ import PHONE_FIELD from '@salesforce/schema/restaurant.phone';
 import PREFERRED_CUISINE_FIELD from '@salesforce/schema/contact.preferredcuisinefield'
 */
 
+
+
 const data = [
-    { id: 1, name: 'Chipotle', cuisine: 'Fast Food', price: '$', rating: 4.0, phone: '212-575-8424' },
+    /*{ id: 1, name: 'Chipotle', cuisine: 'Fast Food', price: '$', rating: 4.0, phone: '212-575-8424' },
     { id: 2, name: 'Chick-fil-A', cuisine: 'Fast Food', price: '$', rating: 5.0, phone: '718-504-6528' },
     { id: 3, name: 'Popeyes', cuisine: 'Fast Food', price: '$', rating: 4.5, phone: '917-475-1546' },
     { id: 4, name: 'Starbucks', cuisine: 'Dessert', price: '$', rating: 3.7, phone: '212-221-7515' },    
+    */
 ];
 
 const columns = [
     { label: 'Restaurant', fieldName: 'name' },
     { label: 'Cuisine', fieldName: 'cuisine', type: 'string' },
-    {
-        label: 'Rating',
-        fieldName: 'rating',
-        type: 'number',
-        sortable: true,
-        cellAttributes: { alignment: 'left' },
-    },    
+    { label: 'Rating',
+      fieldName: 'rating',
+      type: 'number',
+      sortable: true,
+      cellAttributes: { alignment: 'left' }, },    
     { label: 'Price', fieldName: 'price', type: 'string', sortable: true, cellAttributes: { alignment: 'left'  },  },      
     { label: 'Phone', fieldName: 'phone', type: 'string' },
 ];
 
-export default class DemoApp extends LightningElement {
+export default class myModal extends LightningElement {
 
-    name
-    cuisine
-    price
-    rating
-    phone
+    name;
+            //placeholder
+    cuisine = 'Thai';
+    price;
+    rating;
+    phone;
     @api valueFromParentComponent;
     data = data;
     columns = columns;
@@ -44,7 +51,25 @@ export default class DemoApp extends LightningElement {
     sortDirection = 'asc';
     sortedBy;
 
-    // Used to sort the 'Age' column
+
+    handleOptionChange(event) {
+        this.selectedOption = event.detail.value;
+    }
+
+    closeModal() {
+        this.dispatchEvent(new CustomEvent('close'));
+    }
+   
+    customFormModal = false;
+    customShowModalPopup() {
+        this.customFormModal = true;
+    }
+
+    customHideModalPopup() {
+
+        this.customFormModal = false;
+    }
+    //sort the numerical fields
     sortBy(field, reverse, primer) {
         const key = primer
             ? function (x) {
@@ -70,29 +95,53 @@ export default class DemoApp extends LightningElement {
         this.sortDirection = sortDirection;
         this.sortedBy = sortedBy;
     }
+
+    selectYourGirl() {}
+
+
     clickForMoreRestaurants() {
     findRestaurants({
-        data : this.data,
-        name : this.name,
-        cuisine : this.cuisine,
-        price : this.price,
-        rating : this.rating,
-        phone : this.phone
+        
+        preferredCuisine : this.cuisine
     }
 
         
     )
-    var table = $('#example').DataTable();
-    var newRow = [
-        data,
-        name,
-        cuisine,
-        price,
-        rating,
-        phone
-      ];
-    table.row.add(newRow).draw();   
-                
+   
+    wiredData({ error, data }); {
+    if (data) {
+        this.data = data;
+    } else if (error) {
+        // Handle error
+    }
+    };
+
+    addRow(); {
+        this.data.push({
+            name: this.name,
+             cuisine: this.cuisine,
+             price: this.price,
+             rating: this.rating,
+             phone: this.phone
+        });
+    }    
+    // var table = $('#restaurantTable').DataTable();
+    // if (this.name && this.cuisine && this.price && this.rating && this.phone) {
+    // var newRow = {
+    //     name: this.name,
+    //     cuisine: this.cuisine,
+    //     price: this.price,
+    //     rating: this.rating,
+    //     phone: this.phone
+    // };
+    // if (table) {
+    //     table.row.add(newRow).draw();
+    // } else {
+    //     console.log("Error: DataTable is not defined or initialized.");
+    // }
+    // } else {
+    // console.log("Error: One or more required values are missing.");
+    // }            
       
     }
 }
